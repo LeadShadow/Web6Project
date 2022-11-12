6f5903452475b9cbafa1f317d502edbe46751b79
Vitaliy Lobko <vitaliy.lobko@gmail.com>
2022-11-12 10:53:25 +0200

    add parser

--- webproject/project/views.py
+++ webproject/project/views.py
@@ -6,6 +6,11 @@ from django.db import IntegrityError
 from .models import Tag, Note, User, Files
 from .forms import TagForm, NoteForm
 
+import requests
+from bs4 import BeautifulSoup
+import json
+from datetime import datetime
+
 
 # Create your views here.
 
@@ -36,7 +41,6 @@ def user_login(request):
                           {'form': AuthenticationForm(), 'error': 'Username or password didn\'t match'})
         login(request, user)
         return redirect('main')
-
 
 
 @login_required
@@ -106,6 +110,7 @@ def show_notes(request):
         print(sort)
         return render(request, 'project/notes.html', {'back': '/'})
 
+
 @login_required
 def set_done_note(request, note_id):
     Note.objects.filter(pk=note_id, user_id=request.user).update(done=True)
@@ -117,6 +122,7 @@ def delete_note(request, note_id):
     note = Note.objects.get(pk=note_id, user_id=request.user)
     note.delete()
     return redirect('main')
+
 
 @login_required
 def edit_note(request, note_id):
@@ -137,6 +143,7 @@ def edit_note(request, note_id):
             return render(request, 'project/notes.html', {"tags": tags, 'form': NoteForm(), 'error': err})
 
     return render(request, 'project/notes.html', {"tags": tags, 'form': NoteForm(instance=note)})
+
 
 @login_required
 def addressbook(request):
@@ -167,3 +174,83 @@ def show_addressbook(request):
 def download_files(request):
     pass
 
+
+@login_required
+def parser(request):
+    # # CURRENCY
+    # # https://minfin.com.ua/currency/
+    # data = []
+    #
+    # base_url = 'https://minfin.com.ua/currency/'
+    # response = requests.get(base_url)
+    # soup = BeautifulSoup(response.text, 'html.parser')
+    # print(soup)
+    # content = soup.find('table',
+    #                     class_='table-response mfm-table mfcur-table-lg mfcur-table-lg-currency has-no-tfoot').find_all(
+    #     'tr')
+    # for element in content:
+    #     result = {}
+    #     try:
+    #         currency_name = element.find('td', class_='mfcur-table-cur').find('a').text
+    #     except AttributeError:
+    #         continue
+    #     result.update({"currency_name": currency_name})
+    #     rate = element.find('td',
+    #                         attrs={"class": "mfm-text-nowrap", "data-title": "Черный рынок"}).text.strip().replace('\n',
+    #                                                                                                                '')
+    #     print(rate)
+    #     buy = rate.split('/')[0]
+    #     sale = rate.split('/')[1]
+    #     result.update({"buy": buy})
+    #     result.update({"sale": sale})
+    #     result.update({"source": base_url})
+    #     data.append(result)
+    #
+    #     currency = json.dumps(data, ensure_ascii=False)
+
+    # NEWS
+    news = []
+    # https://ua.korrespondent.net/
+    base_url = 'https://ua.korrespondent.net/'
+    response = requests.get(base_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    content = soup.find('div', class_='time-articles').find_all('div', class_='article')
+    for element in content:
+        result = {}
+        dtime = element.find('div', attrs={"class": "article__time"}).text
+        date_time = datetime(datetime.now().year, datetime.now().month, datetime.now().day, int(dtime.split(":")[0]),
+                             int(dtime.split(":")[1]))
+        result.update({"dtime": date_time})
+        title = element.find('div', attrs={"class": "article__title"}).text
+        result.update({"title": title})
+        href = element.find('div', attrs={"class": "article__title"}).find('a').get('href')
+        result.update({"href": href})
+        result.update({"source": base_url})
+        news.append(result)
+
+    # https://news.liga.net/
+    base_url = 'https://news.liga.net/'
+    response = requests.get(base_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    content = soup.find('div', class_='custom-tab-content active').find_all('div', class_='news-nth')
+    for element in content:
+        result = {}
+        try:
+            dtime = element.find('div', attrs={"class": "news-nth-time"}).text
+        except AttributeError:
+            continue
+        date_time = datetime(datetime.now().year, datetime.now().month, datetime.now().day, int(dtime.split(":")[0]),
+                             int(dtime.split(":")[1]))
+        result.update({"dtime": date_time})
+        title = element.find('a').text
+        result.update({"title": title})
+        href = element.find('a').get('href')
+        result.update({"href": href})
+        result.update({"source": base_url})
+        news.append(result)
+
+    # news = json.dumps(data, ensure_ascii=False)
+    weather = []
+    currency = []
+
+    return render(request, 'project/info.html', {"news": news, 'weather': weather, 'currency': currency})
